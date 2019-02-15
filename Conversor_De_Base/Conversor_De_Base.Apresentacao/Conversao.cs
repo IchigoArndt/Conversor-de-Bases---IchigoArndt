e7b6574facfe62f1a5d34ea15b3283a0a1b51fc4
IchigoArndt <arndtfelipe@gmail.com>
2019-02-15 13:59:39 -0200

    Implementado a lógica de conversão entre as bases

---
+++ Conversor_De_Base/Conversor_De_Base.Apresentacao/Conversao.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversor_De_Base.Apresentacao
+{
+   public class Conversao
+    {
+        public List<int> conversao(int numero , int baseNumerica)
+        {
+
+            int resto = 0;
+
+            List<int> binario = new List<int>();
+
+            while (numero >= baseNumerica)
+            {
+                resto = numero % baseNumerica;
+
+                numero = numero / baseNumerica;
+
+                binario.Add(resto);
+            };
+
+            if(resto < baseNumerica &&  resto != 0)
+            {
+                binario.Add(1);
+            }
+
+            binario.Reverse();
+
+            return binario.ToList();
+        }
+    }
+}
---
+++ Conversor_De_Base/Conversor_De_Base.Apresentacao/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conversor_De_Base.Apresentacao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numero = Convert.ToInt32(textBox1.Text);
+            int baseNumerica = Convert.ToInt32(textBox3.Text);
+
+            Conversao bn = new Conversao();
+
+            List<int> binario = new List<int>();
+
+            binario = bn.conversao(numero,baseNumerica);
+
+            string resultado = " ";
+
+            foreach (var item in binario)
+            {
+                resultado += item;
+            }
+
+            textBox2.Text = resultado;
+
+        }
+    }
+}
